6394bb184f7057713fd5f9a7f33036c97ecdcd81
kongs <vividsunmi@snu.ac.kr>
2025-03-24 11:45:49 +0900

    refactor: remove unused provider import from SeatPage

--- lib/pages/seat_page.dart
+++ lib/pages/seat_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
-import '../main.dart';
 
 class SeatPage extends StatefulWidget {
   final String departureStation;
